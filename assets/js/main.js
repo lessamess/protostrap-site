ced18a5d861d97e4664f956413c5b77c51afe8c7
Memi Beltrame <memi@lessamess.ch>
2016-12-24 14:40:07 +0100

    sync assets

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,20 +11,66 @@
             todayHighlight: true
         });
 
-        //livesearch example
-        $('.typeahead').typeahead({
-              source: ['foo bar','foo fighters','a fools errand','football'],
-              limit: 10,
-              updater: function(item){
-                // Do things when selected
-                // console.log(item);
-                return item;
-              }
-        });
+        //LIVESEARCH EXAMPLE in 3 Parts
+                // Documentation: https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md
+                // Doc-Passage on datasets: https://github.com/twitter/typeahead.js/blob/master/doc/jquery_typeahead.md#datasets
 
+            // Part 1: the Matcher Function
+            // LEAVE ALONE
+            var substringMatcher = function(strs) {
+              return function findMatches(q, cb) {
+                var matches, substringRegex;
+
+                // an array that will be populated with substring matches
+                matches = [];
+
+                // regex used to determine if a string contains the substring `q`
+                substrRegex = new RegExp(q, 'i');
+
+                // iterate through the pool of strings and for any string that
+                // contains the substring `q`, add it to the `matches` array
+                $.each(strs, function(i, str) {
+                  if(str.name != undefined ){
+                      if (substrRegex.test(str.name)) {
+                        matches.push(str);
+                      }
+                  } else {
+                    if (substrRegex.test(str)) {
+                        matches.push(str);
+                      }
+                  }
+                });
+
+                cb(matches);
+              };
+            };
+
+            // Part 2: the Source Data
+            var sourcedata = ['foo bar','foo fighters ticket','a fools errand','football'];
+
+
+            // Part 3: the Livesearch itself
+
+            //
+            $('.typeahead').typeahead({
+                      highlight: true,
+                      minLength: 1
+                    },
+                    {
+                      name: 'mytypeahead',
+                      source: substringMatcher(sourcedata),
+                      templates: {
+                        notFound: '<p class="tt-noentries" ><strong>No entries found</strong></p>',
+                        footer: '<p class="tt-footer" ><button class="btn btn-default">Show all results</button></p>',
+                        suggestion: function(data){
+                              return '<p class="topborder"><strong>' + data + '</strong>  <span class="pull-right">$' +  Math.floor((Math.random() * 100) + 1)  + '.99</span></p>';
+                            }
+                        }
+                    });
+
+        // END OF TYPEAHEAD EXAMPLE
 
         $(document).bind('keydown','alt+r', function(){
-
             var url = window.location.href;
             var get = "session_renew=true";
             var start = "?";
@@ -35,5 +81,19 @@
         });
 
         $("#socialsecurity").mask("99-99-9999",{placeholder:"__-__-____"});
+
+        $(".mypopover").popover();
+
+        $(".htmlpopover").popover({
+            container: 'body',
+            html: true,
+            template: '<div class="popover fullwidth" role="tooltip"><div class="arrow"></div><h3 class="popover-title"></h3><div class="fullwidth popover-content"></div></div>',
+            content: function () {
+                var content = $("#"+$(this).data('content-div')).html();
+                return content;
+            }
+        });
+
+
 
     })
