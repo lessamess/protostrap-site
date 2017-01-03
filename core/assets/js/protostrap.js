55967ba32976b77c38b9cf7b8e16ba9fb55eff8f
Memi Beltrame <memi@lessamess.ch>
2017-01-03 11:54:17 +0100

    combined assets

--- core/assets/js/protostrap.js
+++ core/assets/js/protostrap.js
@@ -214,8 +214,6 @@
             $(this).select();
         });
 
-        $(".selectOnLoad").first().select();
-
         $(".stepper .btn-next").click(function() {
             var nextId = $(this).attr("data-nextid");
             var thisId = parseInt(nextId) - 1;
